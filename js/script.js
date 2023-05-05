f6215111c7608ea3ba08c72505a5c246b42262f4
Harwin14 <agungharwinn@gmail.com>
2023-05-05 20:59:08 +0700

    another web basic and learning more css, responsive web design

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Toggle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+};
+
+// Toggle searchbox
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+    searchForm.classList.toggle("active");
+    searchBox.focus();
+    e.preventDefault();
+};
+
+//Klik di luar element
+const hb = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button ");
+
+document.addEventListener("click", function (e) {
+    if (!hb.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove("active");
+    }
+});

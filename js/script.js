dc8ada76092e6a678328100e0a4e181dd99882b9
Harwin14 <agungharwinn@gmail.com>
2023-05-08 20:53:31 +0700

    r

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 
 document.querySelector("#search-button").onclick = (e) => {
-    searchForm.classList.toggle("active");
+    searchForm.classList.toggle("active"); 
     searchBox.focus();  
     e.preventDefault();
 };

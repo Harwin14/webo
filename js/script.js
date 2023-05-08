07ad9732605979645906f7507f7a342fec5590e3
Harwin14 <agungharwinn@gmail.com>
2023-05-08 20:53:39 +0700

    r

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.querySelector("#hamburger-menu").onclick = () => {
 
 // Toggle searchbox
 const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
+const searchBox = document.querySelector("#search-box"); 
 
 document.querySelector("#search-button").onclick = (e) => {
     searchForm.classList.toggle("active"); 
